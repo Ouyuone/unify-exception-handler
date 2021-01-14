f8eecda8fbe037d652712b3cf63b5f395e34a8be
ouyu <461388758@qq.com>
2021-01-14 22:27:45 +0800

    1、优化异常策略类ExceptionStrategy构造方法可加入message消息 2、添加帮助文档

--- src/main/java/com/ouyu/unifyexceptionhandler/UnifyExceptionHandlerApplication.java
+++ src/main/java/com/ouyu/unifyexceptionhandler/UnifyExceptionHandlerApplication.java
@@ -1,17 +1,7 @@
 package com.ouyu.unifyexceptionhandler;
 
-import com.ouyu.unifyexceptionhandler.entity.HandlerType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.InputStream;
-import java.net.URL;
 
 @SpringBootApplication
 //@RestController
@@ -19,7 +9,7 @@ public class UnifyExceptionHandlerApplication {
 
     /*@GetMapping("/test")
     public void ss(){
-        throw  new RuntimeException("sss");
+        throw  new RuntimeException("我是运行异常");
     }
 
     @Bean
